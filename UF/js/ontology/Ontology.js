1083339c58b7a5f492594514828fb329ffe6facd
Confoederatio <htmlprogrammingbn@gmail.com>
2026-02-27 20:24:31 +0000

    extended Date framework

--- UF/js/ontology/Ontology.js
+++ UF/js/ontology/Ontology.js
@@ -5,10 +5,14 @@
 	 * 
 	 * ##### Constructor:
 	 * - `arg0_type="Ontology"`: {@link string} - The Ontology subclass/type to reference.
+	 * - `arg2_options`: {@link Object}
+	 *   - `.worker_type`: {@link string} - The worker from which the Ontology originated
 	 * 
 	 * @type {Ontology}
 	 */
 	global.Ontology = class {
+		static instances = [];
+		
 		/**
 		 * @type {string}
 		 */
@@ -26,7 +30,30 @@
 			let options = (arg2_options) ? arg2_options : {};
 			
 			//Declare local instance variables
+			this.geometries = [];
+			this.state = (state_array) ? state_array : [];
+				if (!Array.isArray(this.state)) {
+					if (typeof this.state === "string") this.state = JSON.parse(this.state);
+					if (typeof this.state === "object") this.state = [this.state];
+				}
+		}
+		
+		getState (arg0_date) {
+			//Convert from parameters
+			let date = (typeof arg0_date === "number") ? new Date(arg0_date) : arg0_date;
+		}
+		
+		toJSON () {
 			
+		}
+		
+		static fromJSON (arg0_json) {
+			
+		}
+		
+		static getOntologyID () {
+			//Return statement
+			return `${Date.now()}_${this.queue.length}`;
 		}
 	};
 }
