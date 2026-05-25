8ef610e7c2ce9b48813132974a0375310c4e46d4
Vis Tacitus <vistacitus@confoederatio.org>
2026-05-25 00:35:54 +0000

    updated NDJSON

--- main.js
+++ main.js
@@ -151,38 +151,38 @@ let win;
   let ve = require("./UF/js/vercengen/engine/vercengen_electron");
   ve.initialiseIPC();
   
-  ve.NDJSON_load("./saves/atlas.naissance").then(() => {
-    ve.NDJSON_diffAll("./saves/atlas.naissance.ndjson", { timestamp: 1005088321 })
+  NDJSON.load("./saves/atlas.naissance").then(() => {
+    NDJSON.diffAll("./saves/atlas.naissance.ndjson", { timestamp: 1005088321 })
     .then(async (v) => {
       for (let i = 0; i < v.length; i++) console.log(v[i].key, v[i].value);
-      let local_value = await ve.NDJSON_getValue("./saves/atlas.naissance.ndjson", "45817001146");
+      let local_value = await NDJSON.getValue("./saves/atlas.naissance.ndjson", "45817001146");
       console.log("Local value:", local_value);
       
       console.time("set_value");
-      await ve.NDJSON_setValue("./saves/atlas.naissance.ndjson", "45817001146", { key: "value", hello: "world" });
-      local_value = await ve.NDJSON_getValue("./saves/atlas.naissance.ndjson", "45817001146");
+      await NDJSON.setValue("./saves/atlas.naissance.ndjson", "45817001146", { key: "value", hello: "world" });
+      local_value = await NDJSON.getValue("./saves/atlas.naissance.ndjson", "45817001146");
       console.timeEnd("set_value");
       console.log("Local value after write:", local_value);
       
       console.time("read_value");
-      let local_value_two = await ve.NDJSON_getValue("./saves/atlas.naissance.ndjson", "34593585401");
+      let local_value_two = await NDJSON.getValue("./saves/atlas.naissance.ndjson", "34593585401");
       console.log("Local value:", local_value_two);
       console.timeEnd("read_value");
       
       console.time("set_value_two");
-      await ve.NDJSON_setValue("./saves/atlas.naissance.ndjson", "34593585401", { key: "value", hello: "world" });
-      local_value_two = await ve.NDJSON_getValue("./saves/atlas.naissance.ndjson", "34593585401");
+      await NDJSON.setValue("./saves/atlas.naissance.ndjson", "34593585401", { key: "value", hello: "world" });
+      local_value_two = await NDJSON.getValue("./saves/atlas.naissance.ndjson", "34593585401");
       console.timeEnd("set_value_two");
       console.log("Local value after write:", local_value_two);
       
       console.time("remove");
-      await ve.NDJSON_removeValue("./saves/atlas.naissance.ndjson", "45817001146");
-      local_value = await ve.NDJSON_getValue("./saves/atlas.naissance.ndjson", "45817001146");
+      await NDJSON.removeValue("./saves/atlas.naissance.ndjson", "45817001146");
+      local_value = await NDJSON.getValue("./saves/atlas.naissance.ndjson", "45817001146");
       console.timeEnd("remove");
       console.log("Local value after remove:", local_value);
       
       console.time("query 1000 GeometryPolygons");
-      local_value = await ve.NDJSON_query("./saves/atlas.naissance.ndjson", { 
+      local_value = await NDJSON.query("./saves/atlas.naissance.ndjson", { 
         query_obj: { class_name: "GeometryPolygon" },
         limit_start: 1000,
         limit_end: 1500
@@ -190,9 +190,9 @@ let win;
       console.timeEnd("query 1000 GeometryPolygons");
       console.log("1000 GeometryPolygons:", local_value.length);
       
-      console.log("Active Workers:", ve.NDJSON_getWorkerPool().length);
+      console.log("Active Workers:", NDJSON.getWorkerPool().length);
       
-      //await ve.NDJSON_save("./saves/atlas.naissance.ndjson");
+      //await NDJSON.save("./saves/atlas.naissance.ndjson");
     });
   });
 }
