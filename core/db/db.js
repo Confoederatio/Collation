0e2f128a7e55bf7f03317e4fae0db57b23f06e67
Aust Kätzchen <austkaetzchen@confoederatio.org>
2026-05-28 06:04:38 +0000

    Update db.js

--- core/db/db.js
+++ core/db/db.js
@@ -2,6 +2,11 @@ if (!global.db) global.db = {};
 
 //Initialise functions
 {
+	/**
+	 * Initialises the DB API for Naissance.
+	 * 
+	 * @returns {Promise<boolean>}
+	 */
 	db.initialise = async function () {
 		//Declare local instance variables
 		let all_function_keys = await Blacktraffic.task("ndjson:get-all-functions");
@@ -11,8 +16,18 @@ if (!global.db) global.db = {};
 				//Return statement
 				return await db.send(all_function_keys[i], ...argn_arguments);
 			};
+		
+		//Return statement
+		return true;
 	};
 	
+	/**
+	 * Internal helper method. Sends a command to DB threads by calling an NDJSON function.
+	 * 
+	 * @param {string} arg0_function_key
+	 * @param {any[]} argn_arguments
+	 * @returns {Promise<*>}
+	 */
 	db.send = async function (arg0_function_key, ...argn_arguments) {
 		//Convert from parameters
 		let function_key = arg0_function_key;
