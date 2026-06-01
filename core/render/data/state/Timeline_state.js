28ad45fa22d24c2cd8b6f4c3bb0191c037bd763d
Aust Kätzchen <austkaetzchen@confoederatio.org>
2026-06-01 07:01:21 +0000

    Update Timeline_state.js

--- core/render/data/state/Timeline_state.js
+++ core/render/data/state/Timeline_state.js
@@ -9,14 +9,35 @@ if (!global.naissance) global.naissance = {};
 	 * 
 	 * @param {string} [arg0_key] - The key to push to the current DALS timeline.
 	 * @param {Object|string} [arg1_json]
-	 *  @param {string} [arg1_json.scope="global"] - The scope to parse the action for.
+	 *  @param {string} [arg1_json.type="global"] - The scope type to parse the action for. Either 'global'/'feature'/'geometry'/'stencil'.
+	 * @param {boolean} [arg2_do_not_push_action=false]
 	 */
-	DALS.Timeline.parseAction = async function (arg0_key, arg1_json) {
+	DALS.Timeline.parseAction = async function (arg0_key, arg1_json, arg2_do_not_push_action) {
 		//Convert from parameters
 		let json = (typeof arg1_json === "string") ?
 			JSON.parse(arg1_json) : arg1_json;
 		
-		//Declare local instance variables
+		//Initialise JSON
+		if (json.options === undefined) json.options = {};
+		if (json.value === undefined) json.value = [];
+		
+		//Iterate over multi-value packet (MVP) and filter it down to superclass single-value packets (SVPs)
+		for (let i = 0; i < json.value.length; i++) {
+			let local_value = json.value[i];
+			
+			if (local_value.type === "global") {
+				if (local_value.refresh_date) await naissance.Renderer.setDate(main.date);
+				if (local_value.set_date) await naissance.Renderer.setDate(local_value.set_date);
+			} else if (local_value.type === "feature") {
+				await proc.feature(json.feature_id, json);
+			} else if (local_value.type === "geometry") {
+				await proc.geometry(json.geometry_id, json);
+			} else if (local_value.type === "stencil") {
+				console.warn(`[WIP] - Stencils are not yet implemented.`);
+			}
+		}
+		
+		//Save action to current timeline if needed
 		
 	};
 	
