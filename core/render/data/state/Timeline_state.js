4e7c39c7ac9a0ab02d114675da58c1302047413c
Aust Kätzchen <austkaetzchen@confoederatio.org>
2026-06-01 07:03:40 +0000

    Update Timeline_state.js

--- core/render/data/state/Timeline_state.js
+++ core/render/data/state/Timeline_state.js
@@ -8,8 +8,10 @@ if (!global.naissance) global.naissance = {};
 	 * Parses a user action inside Naissance. All user actions must be mapped to a valid JSON schema.
 	 * 
 	 * @param {string} [arg0_key] - The key to push to the current DALS timeline.
-	 * @param {Object|string} [arg1_json]
-	 *  @param {string} [arg1_json.type="global"] - The scope type to parse the action for. Either 'global'/'feature'/'geometry'/'stencil'.
+	 * @param {Object|string} [arg1_json] - If no top-level ID is passed, the action is assumed to be global.
+	 *  @param {string} [arg1_json.feature_id] - Top-level ID for {@link naissance.Feature}.
+	 *  @param {string} [arg1_json.geometry_id] - Top-level ID for {@link naissance.Geometry}.
+	 *  @param {string} [arg1_json.stencil_id]
 	 * @param {boolean} [arg2_do_not_push_action=false]
 	 */
 	DALS.Timeline.parseAction = async function (arg0_key, arg1_json, arg2_do_not_push_action) {
@@ -25,15 +27,15 @@ if (!global.naissance) global.naissance = {};
 		for (let i = 0; i < json.value.length; i++) {
 			let local_value = json.value[i];
 			
-			if (local_value.type === "global") {
+			if (local_value.feature_id) {
+				await proc.feature(json.feature_id, json);
+			} else if (local_value.geometry_id) {
+				await proc.geometry(json.geometry_id, json);
+			} else if (local_value.stencil_id) {
+				console.warn(`[WIP] - Stencils are not yet implemented.`);
+			} else {
 				if (local_value.refresh_date) await naissance.Renderer.setDate(main.date);
 				if (local_value.set_date) await naissance.Renderer.setDate(local_value.set_date);
-			} else if (local_value.type === "feature") {
-				await proc.feature(json.feature_id, json);
-			} else if (local_value.type === "geometry") {
-				await proc.geometry(json.geometry_id, json);
-			} else if (local_value.type === "stencil") {
-				console.warn(`[WIP] - Stencils are not yet implemented.`);
 			}
 		}
 		
