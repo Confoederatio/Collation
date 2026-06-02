f89b30f0994c3de0136c6cff7b58d507e22e2e44
Vis Tacitus <vistacitus@confoederatio.org>
2026-06-02 07:08:29 +0000

    Update FeatureAction.js

--- core/process/actions_thread/FeatureAction.js
+++ core/process/actions_thread/FeatureAction.js
@@ -9,6 +9,4 @@ proc.FeatureAction = async function (arg0_json) {
 	//Convert from parameters
 	let json = (arg0_json) ? arg0_json : {};
 	
-	console.log(`Received JSON:`, json);
-	console.log(`Test DB query:`, await db.getValue("35194181118"));
 };
