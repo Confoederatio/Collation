951c71ef2e8c445ae9a98fa7109bbf44ce6812bf
Aust Kätzchen <austkaetzchen@confoederatio.org>
2026-06-20 14:42:12 +0000

    logically, it seems to work

--- core/framework/actions/FeatureLayer_actions.js
+++ core/framework/actions/FeatureLayer_actions.js
@@ -50,168 +50,132 @@ naissance.FeatureLayer.parseAction = async function (arg0_json) {
 			let to_layer_geometries = to_layer.getAllGeometries();
 			let to_layer_timestamps = to_layer.getTimestamps();
 			
-			let all_timestamps = [...new Set([...from_layer_timestamps, ...to_layer_timestamps])];
-			all_timestamps = all_timestamps.sort((a, b) => a - b);
+			let all_timestamps = [...new Set([...from_layer_timestamps, ...to_layer_timestamps])].sort((a, b) => a - b);
 			let end_date = (json.merge_layer.end_date) ?
-				json.merge_layer.end_date : from_layer_timestamps[from_layer_timestamps.length - 1];
-			end_date = Date.getTimestamp(end_date);
+				Date.getTimestamp(json.merge_layer.end_date) : from_layer_timestamps[from_layer_timestamps.length - 1];
 			let start_date = (json.merge_layer.start_date) ?
-				json.merge_layer.start_date : from_layer_timestamps[0];
-			start_date = Date.getTimestamp(start_date);
+				Date.getTimestamp(json.merge_layer.start_date) : from_layer_timestamps[0];
 			
-			//1. Difference Layer A from Layer B
+			//1. Difference Layer A from Layer B to ensure unlinked source polygons have room in to_layer
 			let from_layer_union;
 			
-			for (let i = 0; i < all_timestamps.length; i++)
-				if ((all_timestamps[i] >= start_date && all_timestamps[i] <= end_date)) {
-					//Update from_layer_union
-					if (from_layer_timestamps.includes(all_timestamps[i])) {
+			for (let i = 0; i < all_timestamps.length; i++) {
+				let current_timestamp = all_timestamps[i];
+				if (current_timestamp >= start_date && current_timestamp <= end_date) {
+					//Update the current union of geometries in the source layer at this timestamp
+					if (from_layer_timestamps.includes(current_timestamp)) {
 						from_layer_union = undefined;
 						
 						for (let x = 0; x < from_layer_geometries.length; x++) {
 							let local_geometry = from_layer_geometries[x];
-							let local_keyframe = local_geometry.history.getKeyframe(all_timestamps[i]);
+							let local_keyframe = local_geometry.history.getKeyframe({ date: current_timestamp });
 							
-							if (local_keyframe?.value?.[0])
-								if (local_geometry.class_name === "GeometryPolygon") {
-									let local_maptalks_geometry = maptalks.Geometry.fromJSON(local_keyframe.value[0]);
-									let local_turf_geometry = Geospatiale.convertMaptalksToTurf(local_maptalks_geometry);
-									
-									if (from_layer_union === undefined) {
-										from_layer_union = local_turf_geometry;
-									} else {
-										from_layer_union = turf.union(turf.featureCollection([from_layer_union, local_turf_geometry]));
-									}
-								}
+							if (local_keyframe?.value?.[0] && local_geometry.class_name === "GeometryPolygon") {
+								let maptalks_json = maptalks.Geometry.fromJSON(local_keyframe.value[0]);
+								let turf_geometry = Geospatiale.convertMaptalksToTurf(maptalks_json);
+								
+								from_layer_union = (from_layer_union === undefined) ?
+									turf_geometry : turf.union(turf.featureCollection([from_layer_union, turf_geometry]));
+							}
 						}
 					}
-					//Subtract from_layer_union from to_layer
-					if (from_layer_union && to_layer_timestamps.includes(all_timestamps[i])) {
+					
+					//Subtract the source union from every geometry in the destination layer
+					if (from_layer_union && (from_layer_timestamps.includes(current_timestamp) || to_layer_timestamps.includes(current_timestamp))) {
 						for (let x = 0; x < to_layer_geometries.length; x++) {
-							let has_geometry_keyframe = false;
-							let local_geometry = to_layer_geometries[x];
+							let target_geometry = to_layer_geometries[x];
+							let target_keyframe = target_geometry.history.getKeyframe({ date: current_timestamp });
 							
-							//Guard clause to ensure a valid geometry at this timestamp
-							if (local_geometry?.history.keyframes?.[all_timestamps[i]]?.[0]) has_geometry_keyframe = true;
-							if (!has_geometry_keyframe) continue;
-							
-							let local_keyframe = local_geometry.history.keyframes[all_timestamps[i]];
-							
-							if (local_geometry.class_name === "GeometryPolygon") {
-								let local_maptalks_geometry = maptalks.Geometry.fromJSON(local_keyframe.value[0]);
-								let local_turf_geometry = Geospatiale.convertMaptalksToTurf(local_maptalks_geometry);
+							if (target_keyframe?.value?.[0] && target_geometry.class_name === "GeometryPolygon") {
+								let maptalks_target = maptalks.Geometry.fromJSON(target_keyframe.value[0]);
+								let turf_target = Geospatiale.convertMaptalksToTurf(maptalks_target);
 								
-								local_turf_geometry = turf.difference(turf.featureCollection([local_turf_geometry, from_layer_union]));
-								local_maptalks_geometry = Geospatiale.convertTurfToMaptalks(local_turf_geometry);
+								let differenced_turf = turf.difference(turf.featureCollection([turf_target, from_layer_union]));
+								let maptalks_result = Geospatiale.convertTurfToMaptalks(differenced_turf);
+								let result_json = (maptalks_result && typeof maptalks_result.toJSON === "function") ?
+									maptalks_result.toJSON() : null;
 								
-								if (typeof local_maptalks_geometry.toJSON === "function") {
-									local_keyframe.value[0] = local_maptalks_geometry.toJSON();
-								} else {
-									local_keyframe.value[0] = null;
-								}
+								//Add keyframe to record the subtraction at this time
+								target_geometry.history.addKeyframe(current_timestamp, result_json);
 							}
 						}
 					}
-				}
-			
-			//2. Clean keyframes in Layer B
-			let to_layer_geometry_ids = [];
-			
-			//Iterate over all to_layer_geometries and push their IDs
-			for (let i = 0; i < to_layer_geometries.length; i++)
-				to_layer_geometry_ids.push(to_layer_geometries[i].id);
-			naissance.Geometry.parseActionForGeometries(to_layer_geometry_ids, {
-				command: "clean_keyframes",
-				key: "clean_keyframes",
-				name: "Clean F.Geometry Keyframes",
-				value: []
-			});
-			
-			//3. Union linked polygons in Layer A with Layer B
-			for (let i = 0; i < from_layer_geometries.length; i++)
-				if (from_layer_geometries[i]?.metadata?.linked_id) {
-					let active_from_geometry;
-					let active_to_geometry;
-					let from_geometry = from_layer_geometries[i];
-					let to_geometry = naissance.Geometry.instances[from_layer_geometries[i].metadata.linked_id];
-					
-					if (to_geometry) {
-						let from_keyframes = Object.keys(from_geometry.history.keyframes).map(Number);
-						let to_keyframes = Object.keys(to_geometry.history.keyframes).map(Number);
-						let unique_timestamps = [...new Set([...from_keyframes, ...to_keyframes])];
-						unique_timestamps = unique_timestamps.sort((a, b) => a - b);
-						
-						//Iterate over all unique_timestamps and perform a merge
-						for (let x = 0; x < unique_timestamps.length; x++) {
-							if (unique_timestamps[x] < start_date || unique_timestamps[x] > end_date) continue;
-							
-							let from_keyframe = from_geometry.history.keyframes[unique_timestamps[x]];
-							let to_keyframe = to_geometry.history.keyframes[unique_timestamps[x]];
-							
-							if (from_keyframe)
-								if (from_keyframe.value[0] !== undefined && from_keyframe.value[0] !== "undefined") {
-									let maptalks_geometry = maptalks.Geometry.fromJSON(from_keyframe.value[0]);
-									active_from_geometry = Geospatiale.convertMaptalksToTurf(maptalks_geometry);
-								} else if (from_keyframe.value[0] === null) {
-									active_from_geometry = undefined;
-								}
-							if (to_keyframe)
-								if (to_keyframe.value[0] !== undefined && to_keyframe.value[0] !== "undefined") {
-									let maptalks_geometry = maptalks.Geometry.fromJSON(to_keyframe.value[0]);
-									active_to_geometry = Geospatiale.convertMaptalksToTurf(maptalks_geometry);
-								} else if (to_keyframe.value[0] === null) {
-									active_to_geometry = undefined;
-								}
-							
-							//Perform an inclusive or union based on rolling state
-							if (active_from_geometry && active_to_geometry) {
-								let turf_union = turf.union(turf.featureCollection([active_from_geometry, active_to_geometry]));
-								
-								to_keyframe.value[0] = Geospatiale.convertTurfToMaptalks(turf_union).toJSON();
-							} else if (active_from_geometry || active_to_geometry) {
-								to_geometry.history.addKeyframe(unique_timestamps[x],
-									Geospatiale.convertTurfToMaptalks((active_from_geometry) ?
-										active_from_geometry : active_to_geometry).toJSON());
-							}
-							
-							//Merge values after [0] for from_keyframe
-							if (from_keyframe && from_keyframe.value.length > 1) {
-								let extra_values = [...from_keyframe.value];
-								extra_values.shift(); //Pop geometry coords
-								to_geometry.history.addKeyframe(unique_timestamps[x], undefined, ...extra_values);
-							}
-						}
-					}
-					
-					//Merge complete, delete from_geometry
-					from_geometry.remove(true);
-				}
-			
-			//4. Clip remaining entities from Layer A so they do not have keyframes before or after the start/end dates
-			for (let i = 0; i < layer_obj.entities.length; i++) {
-				let local_entity = layer_obj.entities[i];
-				
-				if (local_entity.class_name.startsWith("Geometry")) {
-					let from_timestamps = local_entity.history.getTimestamps().map(Number);
-					from_timestamps.sort((a, b) => a - b);
-					
-					if (from_timestamps[0] < start_date) {
-						local_entity.history.keyframes[start_date] = local_entity.history.getKeyframe(start_date);
-						
-						//Iterate over all from_timestamps and remove the ones before start_date; after end_date
-						for (let x = 0; x < from_timestamps.length; x++)
-							if (from_timestamps[x] < start_date || from_timestamps[x] > end_date)
-								delete local_entity.history.keyframes[from_timestamps[x]];
-					}
-					if (Object.keys(local_entity.history.keyframes).length === 0) //Remove empty geometries outside the domain
-						local_entity.remove(true);
 				}
 			}
 			
-			//5. Move remaining entities from Layer A into Layer B
-			to_layer.entities = to_layer.entities.concat(layer_obj.entities);
+			//2. Clean target keyframes
+			let to_layer_ids = [];
+			for (let i = 0; i < to_layer_geometries.length; i++) to_layer_ids.push(to_layer_geometries[i].id);
+			naissance.Geometry.parseActionForGeometries(to_layer_ids, {
+				command: "clean_keyframes", key: "clean_keyframes", name: "Clean F.Geometry Keyframes", value: []
+			});
 			
-			//6. Delete Layer B if requested
+			//3. Union linked polygons
+			for (let i = 0; i < from_layer_geometries.length; i++) {
+				let from_geometry = from_layer_geometries[i];
+				if (from_geometry?.metadata?.linked_id) {
+					let to_geometry = naissance.Geometry.instances[from_geometry.metadata.linked_id];
+					if (to_geometry) {
+						let unique_timestamps = [...new Set([...Object.keys(from_geometry.history.keyframes).map(Number), ...Object.keys(to_geometry.history.keyframes).map(Number)])].sort((a, b) => a - b);
+						
+						for (let x = 0; x < unique_timestamps.length; x++) {
+							let current_timestamp = unique_timestamps[x];
+							if (current_timestamp < start_date || current_timestamp > end_date) continue;
+							
+							let from_keyframe = from_geometry.history.keyframes[current_timestamp];
+							let to_keyframe = to_geometry.history.keyframes[current_timestamp];
+							let from_turf = (from_keyframe?.value?.[0]) ? Geospatiale.convertMaptalksToTurf(maptalks.Geometry.fromJSON(from_keyframe.value[0])) : undefined;
+							let to_turf = (to_keyframe?.value?.[0]) ? Geospatiale.convertMaptalksToTurf(maptalks.Geometry.fromJSON(to_keyframe.value[0])) : undefined;
+							
+							if (from_turf && to_turf) {
+								let turf_union = turf.union(turf.featureCollection([from_turf, to_turf]));
+								to_geometry.history.addKeyframe(current_timestamp, Geospatiale.convertTurfToMaptalks(turf_union).toJSON());
+							} else if (from_turf || to_turf) {
+								to_geometry.history.addKeyframe(current_timestamp, Geospatiale.convertTurfToMaptalks(from_turf || to_turf).toJSON());
+							}
+							
+							if (from_keyframe && from_keyframe.value.length > 1) {
+								let extra_values = [...from_keyframe.value];
+								extra_values.shift();
+								to_geometry.history.addKeyframe(current_timestamp, undefined, ...extra_values);
+							}
+						}
+					}
+					from_geometry.remove(true);
+				}
+			}
+			
+			//4. Clip unlinked entities and prepare for transfer
+			for (let i = layer_obj.entities.length - 1; i >= 0; i--) {
+				let local_entity = layer_obj.entities[i];
+				if (local_entity.class_name.startsWith("Geometry")) {
+					let timestamps = local_entity.history.getTimestamps().map(Number).sort((a, b) => a - b);
+					
+					if (timestamps.length > 0) {
+						if (timestamps[0] < start_date) {
+							let start_keyframe = local_entity.history.getKeyframe({ date: start_date });
+							if (start_keyframe) local_entity.history.addKeyframe(start_date, ...JSON.parse(JSON.stringify(start_keyframe.value)));
+						}
+						if (timestamps[timestamps.length - 1] > end_date) {
+							let end_keyframe = local_entity.history.getKeyframe({ date: end_date });
+							if (end_keyframe) local_entity.history.addKeyframe(end_date, ...JSON.parse(JSON.stringify(end_keyframe.value)));
+						}
+						
+						let current_timestamps = local_entity.history.getTimestamps().map(Number);
+						for (let x = 0; x < current_timestamps.length; x++)
+							if (current_timestamps[x] < start_date || current_timestamps[x] > end_date)
+								delete local_entity.history.keyframes[current_timestamps[x]];
+					}
+					
+					if (Object.keys(local_entity.history.keyframes).length === 0) local_entity.remove(true);
+				}
+			}
+			
+			//5. Transfer unlinked entities
+			to_layer.entities = to_layer.entities.concat(layer_obj.entities);
+			layer_obj.entities = [];
+			
+			//6. Final cleanup
 			if (!json.merge_layer.do_not_delete_after) {
 				layer_obj.remove();
 			} else {
