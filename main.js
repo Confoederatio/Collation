8cc261b2140e0be9546a53b6efe7a3fdd6ed3843
Vis Tacitus <vistacitus@confoederatio.org>
2026-06-21 16:10:15 +0000

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let { performance } = require("perf_hooks");
 
 //Metadata - Title
 let latest_fps = 0;
-let naissance_version = "1.83 California";
+let naissance_version = "1.9 California";
 let title_update_interval;
 let win;
 
