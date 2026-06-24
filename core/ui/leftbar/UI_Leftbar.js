1913d528544269824c87206120afc3f83ec250f4
Aust Kätzchen <austkaetzchen@confoederatio.org>
2026-06-24 22:40:37 +0000

    updated tooltip

--- core/ui/leftbar/UI_Leftbar.js
+++ core/ui/leftbar/UI_Leftbar.js
@@ -39,7 +39,7 @@ global.UI_Leftbar = class extends ve.Class {
 							new UI_SystemManagerWindow();
 						}, {
 							name: `<icon>handyman</icon><span style = "padding-left: 0.25rem; padding-right: 0.5rem;">Advanced Tools</span>`,
-							tooltip: `Tools for modelling and extending Naissance, as well as a System Manager for tasks.`,
+							tooltip: `Advanced tools for modelling, scraping, and extending Naissance.`,
 							style: { "#name": { alignItems: "center", display: "flex" }, marginLeft: "0.25rem" }
 						})
 					}),
