22f6f8ca8c8131b35e979563e87ddd00f37f9038
Aust Kätzchen <austkaetzchen@confoederatio.org>
2026-06-25 22:26:26 +0000

    added save snapshot as geoJSON op

--- core/ui/topbar/UI_Navbar.js
+++ core/ui/topbar/UI_Navbar.js
@@ -1,6 +1,8 @@
 global.UI_Navbar = class {
 	constructor () {
 		//Declare local instance variables
+		this.ui = {};
+		
 		this.navbar_el = new ve.Navbar({
 			file: {
 				name: "Project",
@@ -12,6 +14,37 @@ global.UI_Navbar = class {
 			edit: {
 				name: "Edit",
 				
+				save_snapshot_as_geojson: {
+					name: "Save Snapshot (GeoJSON)",
+					onclick: () => {
+						if (this.save_snapshot_window) this.save_snapshot_window.close();
+						this.save_snapshot_window = veWindow({
+							file_path: veFile(this.ui.save_snapshot_file_path, {
+								onuserchange: (v) => this.ui.save_snapshot_file_path = v,
+								save_function: () => {
+									//Declare local instance variables
+									let file_path = (this.ui.save_snapshot_file_path || "autosave.json");
+									let geojson_obj = { type: "FeatureCollection", features: [] };
+									let geometries = main.layers.entity_layer.getGeometries();
+									
+									//Save snapshot
+									geometries.forEach((v) => geojson_obj.features.push(v.toGeoJSON()));
+									veToast(`Saved GeoJSON snapshot to ${file_path}.`);
+									
+									//Return statement
+									return JSON.stringify(geojson_obj);
+								}
+							})
+						}, {
+							name: "Save Snapshot as GeoJSON",
+							can_rename: false,
+							height: "5rem",
+							width: "20rem",
+							x: "50dvw - 10rem",
+							y: "50dvh - 2.5rem"
+						});
+					}
+				},
 				toggle_dev_tools: {
 					name: "Toggle Dev Tools",
 					keybind: "ctrl+i",
