ac4c1f00c779018800028e01c7f17ad6d48cf2f5
Aust Kätzchen <austkaetzchen@confoederatio.org>
2026-07-26 00:46:23 +0000

    Update GeometryLabelEditor.js

--- core/framework/entities/Geometry/GeometryLabelEditor/GeometryLabelEditor.js
+++ core/framework/entities/Geometry/GeometryLabelEditor/GeometryLabelEditor.js
@@ -1,24 +1,30 @@
 if (!global.naissance) global.naissance = {};
 
 /**
+ * - `.geometry`: {@link maptalks.Geometry}
  * - `.label_geometries`: {@link Array}<{@link Object}>
  *   - `.geometry`: {@link naissance.Geometry}
  *   - `.type`: {@link string} - Either 'curved'/'straight'.
  * 
  * @type {naissance.GeometryLabelEditor}
  */
-naissance.GeometryLabelEditor = class {
-	constructor (arg0_label_geometries) {
+naissance.GeometryLabelEditor = class {  //[WIP] - Finish class body
+	constructor (arg0_geometry, arg1_label_geometries) {
 		//Convert from parameters
-		let label_geometries = (arg0_label_geometries) ? arg0_label_geometries : [];
+		let geometry = arg0_geometry;
+		let label_geometries = (arg1_label_geometries) ? arg1_label_geometries : [];
 		
 		//Declare local instance variables
+		this.geometry = geometry;
+		this.interfaces = {};
 		this.label_geometries = [];
-		this.selected_geometries = []; //Indexes of selected_geometries
+		this.selected_geometries = [];
+		this.selected_indexes = []; //Indexes of selected_indexes
 	}
 	
 	addLabelGeometry (arg0_options) {
-		
+		//Convert from parameters
+		let options = (arg0_options) ? arg0_options : {};
 	}
 	
 	/**
@@ -28,14 +34,28 @@ naissance.GeometryLabelEditor = class {
 		
 	}
 	
+	drawLabelGeometryUI (arg0_index) {
+		//Convert from parameters
+		let index = arg0_index;
+		
+		if (this.interfaces[index]) this.interfaces[index].remove();
+		this.interfaces[index] = new ve.Window({
+			
+		}, { name: (this.geometry?.name || "Edit Label") });
+	}
+	
 	removeLabelGeometry (arg0_index) {
 		//Convert from parameters
 		let index = arg0_index;
 		
 		//Splice and remove
+		let selected_index = this.selected_indexes.indexOf(index);
 		let remove_geometry = this.label_geometries[index];
 		
 		if (remove_geometry) {
+			if (selected_index !== -1)
+				this.selected_indexes.splice(selected_index, 1);
+			
 			remove_geometry.geometry.remove();
 			this.label_geometries.splice(index, 1);
 			this.draw();
@@ -46,9 +66,20 @@ naissance.GeometryLabelEditor = class {
 		//Convert from parameters
 		let index = arg0_index;
 		
+		//Add to selected_indexes
+		if (!this.selected_indexes.includes(index))
+			this.selected_indexes.push(index);
 	}
 	
 	remove () {
+		//Iterate over all this.label_geometries; this.selected_geometries and remove them
+		for (let i = 0; i < this.label_geometries.length; i++)
+			this.label_geometries[i].geometry.remove();
+		for (let i = 0; i < this.selected_geometries.length; i++)
+			this.selected_geometries[i].remove();
+		this.selected_geometries = [];
 		
+		//Iterate over all this.interfaces and remove local_value
+		Object.iterate(this.interfaces, (local_key, local_value) => local_value.remove());
 	}
 };
