b80d0420412c19c189c55963e10d9b048cf6d8a0
Vis Tacitus <vistacitus@confoederatio.org>
2026-08-28 04:22:14 +0000

    add nominal OLS processing

--- histmap/2.data_cleaning/GDP_nominal_OLS/GDP_nominal_OLS.js
+++ histmap/2.data_cleaning/GDP_nominal_OLS/GDP_nominal_OLS.js
@@ -1,6 +1,6 @@
-global.GDP_PPP_OLS = class {
-	static bf = `${h2}/GDP_PPP_OLS/`;
-	static input_coefficients_json = () => `${GDP_PPP_SEDAC.intermediate_ols_folder}processed_base_model.json`;
+global.GDP_nominal_OLS = class {
+	static bf = `${h2}/GDP_nominal_OLS/`;
+	static input_coefficients_json = () => `${GDP_nominal_SEDAC.intermediate_ols_folder}processed_base_model.json`;
 	static input_covariates_obj = () => GDP_PPP_SEDAC.covariates_obj;
 	static output_ols_folder = `${this.bf}rasters/`;
 	
@@ -9,7 +9,7 @@ global.GDP_PPP_OLS = class {
 	static hf1 = (y) => landuse_HYDE._getHYDEYearName(y);
 	static sf = () => population_Stadester_Legacy;
 	
-	static async A_generateOLS_GDP_PPPRaster (arg0_year) {
+	static async A_generateOLS_GDPRaster (arg0_year) {
 		//Convert from parameters
 		let year = arg0_year;
 		
@@ -17,7 +17,7 @@ global.GDP_PPP_OLS = class {
 		let landarea_raster = GeoPNG.loadNumberRasterImage(metadata_HYDE.input_raster_land_area, {
 			format: "int32"
 		});
-		let output_file_path = `${this.output_ols_folder}OLS_GDP_PPP_${year}.png`;
+		let output_file_path = `${this.output_ols_folder}OLS_GDP_${year}.png`;
 		
 		//Return statement
 		return Statistics.generateOLSRaster(output_file_path, {
@@ -36,13 +36,13 @@ global.GDP_PPP_OLS = class {
 		});
 	}
 	
-	static async A_generateOLS_GDP_PPPRasters () {
+	static async A_generateOLS_GDPRasters () {
 		//Declare local instance variables
 		let hyde_years = landuse_HYDE.sorted_hyde_years;
 		
-		//Iterate over all hyde_years and call A_generateOLS_GDP_PPPRaster
+		//Iterate over all hyde_years and call A_generateOLS_GDPRaster
 		for (let i = 0; i < hyde_years.length; i++) {
-			await this.A_generateOLS_GDP_PPPRaster(hyde_years[i]);
+			await this.A_generateOLS_GDPRaster(hyde_years[i]);
 			await Blacktraffic.yield();
 		}
 	}
@@ -55,6 +55,6 @@ global.GDP_PPP_OLS = class {
 		if (!options.exclude) options.exclude = [];
 		
 		//1. Generate OLS rasters
-		if (!options.exclude.includes("A")) await this.A_generateOLS_GDP_PPPRasters();
+		if (!options.exclude.includes("A")) await this.A_generateOLS_GDPRasters();
 	}
 };
