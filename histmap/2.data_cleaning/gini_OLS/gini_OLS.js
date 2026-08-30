9af02e3326f71b937666bbd78c52b940c04e1480
Vis Tacitus <vistacitus@confoederatio.org>
2026-08-30 10:05:34 +0000

    fixed eoscala transform

--- histmap/2.data_cleaning/gini_OLS/gini_OLS.js
+++ histmap/2.data_cleaning/gini_OLS/gini_OLS.js
@@ -1,11 +1,30 @@
 global.gini_OLS = class {
+	static bf = `${h2}/gini_OLS/`;
 	static input_gini_premodern_csv = `${h1}/gini_Eoscala/`;
+	static intermediate_ols_eoscala = `${this.bf}OLS_Eoscala/`;
+	static intermediate_ols_gapminder = `${this.bf}OLS_Gapminder/`;
+	static intermediate_ols_subngini = `${this.bf}OLS_SubNGini/`;
 	
-	static async A_prepareCovariateSeries () {
+	static options = {
+		eoscala_domain: [-21500, 2006],
+		gapminder_domain: [1800, 1990],
+		subngini_domain: [1990, 2023]
+	};
+	
+	static async getEoscalaGiniObject () {
 		
 	}
 	
-	static async A_prepareCovariates () {
+	static async getGapminderGiniObject () {
+		
+	}
+	
+	static async getSubNGiniObject () {
+		
+	}
+	
+	//[WIP] - Util method to fetch [x, y] from [lng, lat] coordinates? Make sure it snaps to nearest land pixel
+	static getNearestLandPixel (arg0_lng, arg1_lat) {
 		
 	}
 };
--- histmap/3.data_transform/GDP_Eoscala.transform/GDP_Eoscala_transform.js
+++ histmap/3.data_transform/GDP_Eoscala.transform/GDP_Eoscala_transform.js
@@ -25,14 +25,14 @@ global.GDP_Eoscala_transform = class {
 		console.log(`Generating delta series for ${this.delta_GDP_PPP_folder} ..`);
 		await GeoPNG.generateDeltaSeries(this.delta_GDP_PPP_folder, {
 			input_format: "float32",
-			input_format_function: (y) => `${GDP_PPP_pc.intermediate_gdp_scaled_to_national}GDP_PPP_${y}.png`,
+			input_format_function: (y) => `${GDP_PPP_pc.intermediate_gdp_ppp_scaled_to_national}GDP_PPP_${y}.png`,
 			prefix: "delta_GDP_PPP_",
 			years: hyde_years
 		});
 		console.log(`Generating delta series for ${this.delta_GDP_PPP_pc_folder} ..`);
 		await GeoPNG.generateDeltaSeries(this.delta_GDP_PPP_pc_folder, {
 			input_format: "float32",
-			input_format_function: (y) => `${GDP_PPP_pc.output_gdp_pc_folder}GDP_PPP_pc_${y}.png`,
+			input_format_function: (y) => `${GDP_PPP_pc.output_gdp_ppp_pc_folder}GDP_PPP_pc_${y}.png`,
 			prefix: "delta_GDP_PPP_pc_",
 			years: hyde_years
 		});
